ab416b335b7433db26249f59bbd157340a408946
RikiGL <riki.guallichico@epn.edu.ec>
2024-09-17 20:07:21 -0500

    EjercicioFinalizado

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,19 @@
-print("hola mundo")
+'''
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+'''
+# Resolucion
+for i in range (101):
+    
+    if i % 3 == 0 and i % 5 == 0:
+        print(f'{i} FizzBuzz')
+    elif i % 3 == 0:
+        print(f'{i} Fizz')
+    elif i % 5 == 0:
+        print(f'{i} Buzz')     
+    else:
+        print(i)   
